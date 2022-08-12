8cf2bc37b7b08b64b7f7eea5b3dade5de3427310
Рустам Астафеев <astafeev0308@gmail.com>
2022-08-12 18:23:31 +0500

    mnogo fixes

--- administration/urls/site_urls.py
+++ administration/urls/site_urls.py
@@ -4,14 +4,4 @@ from administration.views import site_views
 
 urlpatterns = [
     path('', site_views.Index.as_view(), name='index'),
-
-    path('departments/create', site_views.DepartmentCreateView.as_view(), name='department_create'),
-    path('departments/', site_views.DepartmentListView.as_view(), name='departments_list'),
-    path('departments/<int:pk>', site_views.DepartmentDetailView.as_view(), name='department_detail'),
-    path('departments/<int:pk>/delete', site_views.DepartmentDeleteView.as_view(), name='department_delete'),
-
-    path('positions/create', site_views.PositionCreateView.as_view(), name='position_create'),
-    path('positions/', site_views.PositionListView.as_view(), name='positions_list'),
-    path('positions/<int:pk>', site_views.PositionDetailView.as_view(), name='position_detail'),
-    path('positions/<int:pk>/delete', site_views.PositionDeleteView.as_view(), name='position_delete'),
 ]
--- administration/views/api_views.py
+++ administration/views/api_views.py
@@ -1,8 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import generics
 
-from administration.models import Department, Position
-from administration.serializers import DepartmentSerializer, PositionSerializer
 from authentication.serializers import UserSerializer
 from utils.views import DestroyAPIView, RetrieveAPIView, UpdateAPIView, CreateAPIView
 
@@ -33,52 +31,3 @@ class UserUpdate(UpdateAPIView):
 class UserDelete(DestroyAPIView):
     queryset = User.objects.filter(is_deleted=False)
 
-
-class DepartmentsList(generics.ListAPIView):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
-    search_fields = ['name', ]
-    filterset_fields = ['parent_department_id', 'is_active', 'is_deleted', ]
-
-
-class DepartmentDetail(RetrieveAPIView):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
-
-
-class DepartmentCreate(generics.CreateAPIView):
-    serializer_class = DepartmentSerializer
-
-
-class DepartmentUpdate(UpdateAPIView):
-    queryset = Department.objects.all()
-    serializer_class = DepartmentSerializer
-
-
-class DepartmentDelete(DestroyAPIView):
-    queryset = Department.objects.filter(is_deleted=False)
-
-
-class PositionList(generics.ListAPIView):
-    queryset = Position.objects.all()
-    serializer_class = PositionSerializer
-    search_fields = ['name', ]
-    filterset_fields = ['parent_position_id', 'is_head', 'is_active', 'is_deleted', ]
-
-
-class PositionDetail(RetrieveAPIView):
-    queryset = Position.objects.all()
-    serializer_class = PositionSerializer
-
-
-class PositionCreate(generics.CreateAPIView):
-    serializer_class = PositionSerializer
-
-
-class PositionUpdate(UpdateAPIView):
-    queryset = Position.objects.all()
-    serializer_class = PositionSerializer
-
-
-class PositionDelete(DestroyAPIView):
-    queryset = Position.objects.filter(is_deleted=False)
