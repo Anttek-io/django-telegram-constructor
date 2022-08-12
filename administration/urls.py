04485ee9c69c278e76dd14288236b926dbfb6d7d
Рустам Астафеев <astafeev0308@gmail.com>
2022-08-12 18:59:28 +0500

    small changes

--- administration/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from administration import views
-
-urlpatterns = [
-    path('', views.index_view, name="index"),
-]
--- logger/admin.py
+++ logger/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from logger.models import CustomLogEntry
+
+
+@admin.register(CustomLogEntry)
+class CustomLogEntryAdmin(admin.ModelAdmin):
+    date_hierarchy = 'action_time'
+
+    list_filter = ['user', 'content_type', 'action_flag', ]
+
+    search_fields = ['object_repr', 'change_message', ]
+
+    list_display = ['action_time', 'user', 'content_type', 'action_flag', ]
